d8d7baf5ff1e7c670c46060e3a304ef09765e6b3
iit-federico-bozzini <federico.bozzini@iit.it>
2016-01-14 11:25:19 +0100

    Refactor. Removed unused code.

--- server/main.js
+++ server/main.js
@@ -1,5 +1,4 @@
 var path = require('path');
-var url = require('url');
 var express = require('express');
 var cons = require('consolidate');
 var cookieParser = require('cookie-parser');
@@ -22,11 +21,9 @@ config.ldap = require("./ldap.json");
 
 
 var globalConfig = {
-    minify: process.env.MINIFY == 'yes' ? true : false,
     environment: process.env.ENVIRONMENT || 'local'
 };
 
-var rootPath = path.dirname(__dirname);
 var port = Number(process.env.PORT || 9999);
 
 passport.use(new LdapStrategy(config.ldap,
@@ -39,7 +36,6 @@ app.use(session({
     secret: 'frecht',
     cookie: {
         maxAge: 60 * 1000 * 60 //1 hour
-//        maxAge: 20 * 1000 //10 seconds, debug
     },
     resave: false,
     saveUninitialized: false
@@ -60,7 +56,6 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.set('views', path.join(rootPath, 'server'));
 app.engine('html', cons.handlebars);
 app.set('view engine', 'html');
 var models = require('require-directory')(module, './models');
@@ -70,7 +65,7 @@ for (var modelName in models) {
     app.set(modelName, models[modelName]);
 }
 
-if (globalConfig.environment == 'local') {
+if (globalConfig.environment === 'local') {
     app.use(require('connect-livereload')());
 }
 
@@ -98,30 +93,10 @@ var isLoggedIn = function (req, res, next) {
     }
 };
 
-app.use(function (req, res, next) {
-    var config = configFromReq(req);
-    var parsedUrl = url.parse(req.url);
-    var splittedPath = parsedUrl.pathname.split(path.sep);
-
-    if (splittedPath[1]) {
-        var fileExtension = getFileExtension(parsedUrl.pathname);
-        if (fileExtension == 'js' || fileExtension == 'css') {
-            addPathPrefix(splittedPath, getMinPrefix(config));
-        }
-    }
-
-    parsedUrl.pathname = splittedPath.join(path.sep);
-    req.url = url.format(parsedUrl);
-
-    req.config = config;
-    next();
-});
-
-//app.use('/', express.static(path.join(rootPath, 'dist/app')));
 app.use(express.static('dist/app'));
 
 app.get('/', function (req, res) {
-    renderIndex(req.config, res);
+    res.render(path.join(__dirname, 'views/index'));
 });
 
 app.use('/api/login', function (req, res, next) {
@@ -180,25 +155,3 @@ app.delete('/api/meetings/:id', isLoggedIn, controllers.deleteMeeting);
 app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
-
-function renderIndex(config, res) {
-    res.render(getMinPrefix(config) + 'views/index');
-}
-
-function configFromReq(req) {
-    var config = {};
-    config.minify = req.cookies.minify == 'true' ? true : false;
-    return config;
-}
-
-function getMinPrefix(conf) {
-    return conf.minify || globalConfig.minify ? '' : '';
-}
-
-function addPathPrefix(filePath, prefix) {
-    filePath.splice(1, 0, prefix);
-}
-
-function getFileExtension(filePath) {
-    return filePath.split('.').pop();
-}
